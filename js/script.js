86c99709d2a59d2d11aec217676c4db940fbb68e
Adeniyi Olaitan <adeniyi.olaitanhector@yahoo.com>
2021-06-30 12:51:18 +0100

    added animations and loader

--- js/script.js
+++ js/script.js
@@ -1,22 +1,18 @@
-$(document).ready(function() {
-    $("input, textarea").on({
-        focus: function(){
-        $(this).css("background-color", "#eee");
-        $(this).css("border-radius", "10px")
-        },
-        blur : function(){
-        $(this).css("background-color", "transparent");
-        }
-    })
-    $(".closeNav").click(function(){
-        $(".mNav").css("width", "0%");
-
-    })
-    $(".navBtn").click(function(){
-        $(".mNav").css("width", "100%");
-    })
-    $(".mNav li").click(function(){
-        $(".mNav").css("width", "0");
-    })
+$("input, textarea").on({
+  focus: function () {
+    $(this).css("background-color", "#eee");
+    $(this).css("border-radius", "10px");
+  },
+  blur: function () {
+    $(this).css("background-color", "transparent");
+  },
 });
-
+$(".closeNav").click(function () {
+  $(".mNav").css("width", "0%");
+});
+$(".navBtn").click(function () {
+  $(".mNav").css("width", "100%");
+});
+$(".mNav li").click(function () {
+  $(".mNav").css("width", "0");
+});
